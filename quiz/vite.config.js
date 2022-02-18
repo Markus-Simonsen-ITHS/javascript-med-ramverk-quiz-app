f3dbc9a7b6676a4c8e0e6a4982d4da3b7c20a2ab
Markus Simonsen <33492046+Markussim@users.noreply.github.com>
2022-02-18 09:06:52 +0100

    Added auto formater and formatted all files

--- quiz/vite.config.js
+++ quiz/vite.config.js
@@ -1,47 +1,47 @@
-import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
+import vue from "@vitejs/plugin-vue"
 
 export default defineConfig({
   plugins: [
     VitePWA({
       manifest: {
-        background_color: 'grey',
-        display: 'standalone',
+        background_color: "grey",
+        display: "standalone",
         icons: [
           {
-            purpose: 'any',
-            sizes: '192x192',
-            src: 'manifest-icon-192.maskable.png',
-            type: 'image/png'
+            purpose: "any",
+            sizes: "192x192",
+            src: "manifest-icon-192.maskable.png",
+            type: "image/png"
           },
           {
-            purpose: 'maskable',
-            sizes: '192x192',
-            src: 'manifest-icon-192.maskable.png',
-            type: 'image/png'
+            purpose: "maskable",
+            sizes: "192x192",
+            src: "manifest-icon-192.maskable.png",
+            type: "image/png"
           },
           {
-            purpose: 'any',
-            sizes: '512x512',
-            src: 'manifest-icon-512.maskable.png',
-            type: 'image/png'
+            purpose: "any",
+            sizes: "512x512",
+            src: "manifest-icon-512.maskable.png",
+            type: "image/png"
           },
           {
-            purpose: 'maskable',
-            sizes: '512x512',
-            src: 'manifest-icon-512.maskable.png',
-            type: 'image/png'
+            purpose: "maskable",
+            sizes: "512x512",
+            src: "manifest-icon-512.maskable.png",
+            type: "image/png"
           }
         ],
-        name: 'Quiz App',
-        short_name: 'Quiz',
-        start_url: '.',
-        theme_color: 'lightblue'
+        name: "Quiz App",
+        short_name: "Quiz",
+        start_url: ".",
+        theme_color: "lightblue"
       },
-      registerType: 'autoUpdate',
+      registerType: "autoUpdate",
       workbox: {
-        globPatterns: ['assets/**', 'index.html', 'manifest.webmanifest']
+        globPatterns: ["assets/**", "index.html", "manifest.webmanifest"]
       }
     }),
     vue()
